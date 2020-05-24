897eec3e9800a2056a33bb69c057edb098357e65
Andrea Camilloni <s1090325@studenti.univpm.it>
2020-05-24 16:20:16 +0200

    Download di trends disponibili e closetTrends.

--- OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/dbConnection/TrendsDownload.java
+++ OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/dbConnection/TrendsDownload.java
@@ -2,6 +2,17 @@ package it.univpm.OOP2020.TwitterTrends.dbConnection;
 
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import it.univpm.OOP2020.TwitterTrends.model.TrendModel;
+import model.Example;
 
 public class TrendsDownload {
 	private String url;
@@ -11,19 +22,39 @@ public class TrendsDownload {
 	}
 
 	public String[] TrendsCloset() {
-		url = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535";
+		JSONParser parser = new JSONParser();
 		try {
-			URLConnection urlConnection = new URL(url).openConnection();
+			//URLConnection urlConnection = new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535").openConnection();
+			String jsonString = new Scanner(new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535").openStream(), "UTF-8").useDelimiter("\\A").next();
 			
-
+			JSONArray jsonOB = (JSONArray) parser.parse(jsonString);
+			System.out.print(jsonOB);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		
 	}
 	public String[] TrendsAvailable() {
+		JSONParser parser = new JSONParser();
+		try {
+			//URLConnection urlConnection = new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535").openConnection();
+			String jsonString = new Scanner(new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/available.json").openStream(), "UTF-8").useDelimiter("\\A").next();
+			
+			JSONArray jsonList = (JSONArray) parser.parse(jsonString);
+			//System.out.print(jsonOB);
+			
+			ObjectMapper mapper = new ObjectMapper();
+			 List<TrendModel> trendsAvailable = new ObjectMapper().readValue(jsonList.toString(), new TypeReference<List<TrendModel>>() {});
+			 for (TrendModel trends : trendsAvailable) {
+				 System.out.println(trends);
+				
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 
 	}
