ce549b7e9b3c5e44596aec571d10ea94f43667c1
Andrea Camilloni <s1090325@studenti.univpm.it>
2020-05-24 10:44:35 +0200

    Caricamento progetto

---
+++ OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/Application.java
@@ -0,0 +1,13 @@
+package it.univpm.OOP2020.TwitterTrends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
