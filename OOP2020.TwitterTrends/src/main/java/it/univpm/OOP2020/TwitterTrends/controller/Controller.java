610db20dcc305f8a2827fa256f84d409ba78682b
Andrea Camilloni <s1090325@studenti.univpm.it>
2020-05-25 17:58:59 +0200

    Aggiunta Get/TrendsAvailable

--- OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/controller/Controller.java
+++ OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/controller/Controller.java
@@ -1,8 +1,30 @@
 package it.univpm.OOP2020.TwitterTrends.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import it.univpm.OOP2020.TwitterTrends.dbConnection.TrendsDownload;
+import it.univpm.OOP2020.TwitterTrends.model.TrendModel;
+
 
 @RestController
 public class Controller {
+	/**
+	 * GET /TrendsAvailable 
+	 * @return Lista delle Location per cui Twitter dispone dei Trend
+	 */
 
+	@GetMapping("/TrendsAvailable")
+	public List<TrendModel> TrendsAvailable(){
+		
+		return new TrendsDownload().getTrendsAvailable(); 
+	}
+	
 }
--- OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/dbConnection/TrendsDownload.java
+++ OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/dbConnection/TrendsDownload.java
@@ -2,6 +2,7 @@ package it.univpm.OOP2020.TwitterTrends.dbConnection;
 
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,51 +12,64 @@ import org.json.simple.parser.JSONParser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import it.univpm.OOP2020.TwitterTrends.model.Metadata;
 import it.univpm.OOP2020.TwitterTrends.model.TrendModel;
-import model.Example;
 
 public class TrendsDownload {
-	private String url;
-
+	//private String url;
+	List<TrendModel> trendsAvailable;
+	
 	public TrendsDownload() {
 
 	}
 
-	public String[] TrendsCloset() {
+
+	/*public String[] TrendsCloset() {
 		JSONParser parser = new JSONParser();
 		try {
-			//URLConnection urlConnection = new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535").openConnection();
-			String jsonString = new Scanner(new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535").openStream(), "UTF-8").useDelimiter("\\A").next();
-			
+			// URLConnection urlConnection = new
+			// URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535").openConnection();
+			String jsonString = new Scanner(new URL(
+					"https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535")
+							.openStream(),
+					"UTF-8").useDelimiter("\\A").next();
+
 			JSONArray jsonOB = (JSONArray) parser.parse(jsonString);
 			System.out.print(jsonOB);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
-	public String[] TrendsAvailable() {
+
+	}*/
+
+	public List<TrendModel> getTrendsAvailable() {
 		JSONParser parser = new JSONParser();
 		try {
-			//URLConnection urlConnection = new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535").openConnection();
-			String jsonString = new Scanner(new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/available.json").openStream(), "UTF-8").useDelimiter("\\A").next();
-			
+			// URLConnection urlConnection = new
+			// URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535").openConnection();
+			String jsonString = new Scanner(
+					new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/available.json")
+							.openStream(),
+					"UTF-8").useDelimiter("\\A").next();
+
 			JSONArray jsonList = (JSONArray) parser.parse(jsonString);
-			//System.out.print(jsonOB);
-			
+			// System.out.print(jsonOB);
+
 			ObjectMapper mapper = new ObjectMapper();
-			 List<TrendModel> trendsAvailable = new ObjectMapper().readValue(jsonList.toString(), new TypeReference<List<TrendModel>>() {});
-			 for (TrendModel trends : trendsAvailable) {
-				 System.out.println(trends);
-				
+			trendsAvailable = new ObjectMapper().readValue(jsonList.toString(),
+					new TypeReference<List<TrendModel>>() {
+					});
+			for (TrendModel trends : trendsAvailable) {
+				System.out.println(trends);
+
 			}
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		return trendsAvailable;
 
 	}
 
