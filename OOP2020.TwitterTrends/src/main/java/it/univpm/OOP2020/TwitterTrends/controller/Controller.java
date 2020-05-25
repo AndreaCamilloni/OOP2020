a66f4dfcd37560c7a6c5848facbaf9ae0f20b9ce
Andrea Camilloni <s1090325@studenti.univpm.it>
2020-05-25 19:18:05 +0200

    Aggiunta Get/Metadata:apportare modifiche al campo PlaceType(metadata)

--- OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/controller/Controller.java
+++ OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/controller/Controller.java
@@ -1,5 +1,6 @@
 package it.univpm.OOP2020.TwitterTrends.controller;
 
+
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,19 +9,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import it.univpm.OOP2020.TwitterTrends.dbConnection.TrendsDownload;
+import it.univpm.OOP2020.TwitterTrends.model.Metadata;
 import it.univpm.OOP2020.TwitterTrends.model.TrendModel;
 
 
 @RestController
 public class Controller {
+	
+	
+	@GetMapping("/Metadata")
+	public List<Metadata> getMetadata(){
+		return TrendsDownload.getMetadata(); 
+	}
+
 	/**
 	 * GET /TrendsAvailable 
 	 * @return Lista delle Location per cui Twitter dispone dei Trend
 	 */
-
 	@GetMapping("/TrendsAvailable")
 	public List<TrendModel> TrendsAvailable(){
 		
--- OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/dbConnection/TrendsDownload.java
+++ OOP2020.TwitterTrends/src/main/java/it/univpm/OOP2020/TwitterTrends/dbConnection/TrendsDownload.java
@@ -16,32 +16,28 @@ import it.univpm.OOP2020.TwitterTrends.model.Metadata;
 import it.univpm.OOP2020.TwitterTrends.model.TrendModel;
 
 public class TrendsDownload {
-	//private String url;
-	List<TrendModel> trendsAvailable;
-	
+	// private String url;
+	private static List<TrendModel> trendsAvailable;
+	private static List<Metadata> metadata=new JSONArray() ;
+
 	public TrendsDownload() {
 
 	}
 
-
-	/*public String[] TrendsCloset() {
-		JSONParser parser = new JSONParser();
-		try {
-			// URLConnection urlConnection = new
-			// URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535").openConnection();
-			String jsonString = new Scanner(new URL(
-					"https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535")
-							.openStream(),
-					"UTF-8").useDelimiter("\\A").next();
-
-			JSONArray jsonOB = (JSONArray) parser.parse(jsonString);
-			System.out.print(jsonOB);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}*/
+	/*
+	 * public String[] TrendsCloset() { JSONParser parser = new JSONParser(); try {
+	 * // URLConnection urlConnection = new // URL(
+	 * "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535"
+	 * ).openConnection(); String jsonString = new Scanner(new URL(
+	 * "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/trends/closest.json?lat=43.508321&long=13.376535")
+	 * .openStream(), "UTF-8").useDelimiter("\\A").next();
+	 * 
+	 * JSONArray jsonOB = (JSONArray) parser.parse(jsonString);
+	 * System.out.print(jsonOB); } catch (Exception e) { // TODO Auto-generated
+	 * catch block e.printStackTrace(); }
+	 * 
+	 * }
+	 */
 
 	public List<TrendModel> getTrendsAvailable() {
 		JSONParser parser = new JSONParser();
@@ -57,13 +53,8 @@ public class TrendsDownload {
 			// System.out.print(jsonOB);
 
 			ObjectMapper mapper = new ObjectMapper();
-			trendsAvailable = new ObjectMapper().readValue(jsonList.toString(),
-					new TypeReference<List<TrendModel>>() {
-					});
-			for (TrendModel trends : trendsAvailable) {
-				System.out.println(trends);
-
-			}
+			trendsAvailable = new ObjectMapper().readValue(jsonList.toString(), new TypeReference<List<TrendModel>>() {
+			});
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -71,6 +62,18 @@ public class TrendsDownload {
 		}
 		return trendsAvailable;
 
+	}
+
+	public static List<Metadata> getMetadata() {
+		metadata.add(new Metadata("name", "name", "String"));
+		metadata.add(new Metadata("PlaceType", "placeType", "Object"));
+		metadata.add(new Metadata("url", "url", "String"));
+		metadata.add(new Metadata("parentid", "parentid", "Integer"));
+		metadata.add(new Metadata("country", "country", "String"));
+		metadata.add(new Metadata("woeid", "woeid", "Integer"));
+		metadata.add(new Metadata("countryCode", "countryCode", "String"));
+
+		return metadata;
 	}
 
 }
